72ef15edc1d8748c4821c90eca84fcf997ac6b54
Truta Patricia <truta_georgiana@yahoo.com>
2026-06-14 02:32:36 +0300

    Added emblema cum trebuie

--- src/App.jsx
+++ src/App.jsx
@@ -93,9 +93,7 @@ export default function App() {
   return (
     <>
       <Confetti trigger={confettiBurst} />
-      {inGame && (
-        <img className="game-watermark" src="/logo.png" alt="SODO Kids Academy" />
-      )}
+      <img className="game-watermark" src="/logo.png" alt="SODO Kids Academy" />
       <header className="header">
         {inGame && (
           <button className="back-btn" onClick={() => setScreen('home')}>
